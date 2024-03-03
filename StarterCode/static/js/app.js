93f9051f91a2a6fc80a325b4187a10f47bdd3dec
AAlbers341 <149892097+AAlbers341@users.noreply.github.com>
2024-03-03 12:45:56 -0600

    finial bar chart commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,28 +6,67 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 function init() {
     // Read in the samples.json data
     d3.json(url).then((data) => {
+        // Ensure data was feteched
+        console.log(data)
+
         //set variable for sample names
-        let sampleIds = data.names;
+        let IDs = data.names;
 
         // Populate the dropdown menu with sample IDs
         let dropdownMenu = d3.select("#selDataset");
        
         // For each sample ID value in the array append to dropdownMenu variable
-        sampleIds.forEach(sampleId => {
-            dropdownMenu.append("option").text(sampleId).attr("value", sampleId);
+        IDs.forEach(ID => {
+            dropdownMenu.append("option").text(ID).attr("value", ID);
         });
 
          // Set default sample as the first index in the array
-         let defaultSample = sampleIds[0]; 
+         let defaultSample = IDs[0]; 
 
          // Create initial horizontal bar chart, bubble chart and metadata panel
-        createBar(defaultSample, data);
-        createBubble(defaultSample, data);
-        createMetadata(defaultSample, data);
+        executeBar(defaultSample, data);
+        executeBubble(defaultSample, data);
+        executeMetadata(defaultSample, data);
       
      });    
     
 };
+
+// Function to create horizontal bar chart to display the top 10 OTUs found in that individual
+function executeBar(sampleId,data){
+
+    // Find if the users selection exactly matches the ID
+    let sample = data.samples.find(sample => sample.id === sampleId);
+    
+    // Slice top 10 IDs by sorting the order of elements in the array by .reserve()
+    let sampleValues = sample.sample_values.slice(0, 10).reverse();
+    let otuIds = sample.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(); // Function to take each sliced 'otu_ids' and append 'OTU' transforming it into a meaning full string to plot 
+    let otuLabels = sample.otu_labels.slice(0, 10).reverse();
+
+    // Trace for bar horizontal bar chart
+    let trace = [{
+        x: sampleValues,
+        y: otuIds,
+        text: otuLabels, // Hover text
+        type: "bar",
+        orientation: "h"
+    }];
+
+    // Layout for horizontal bar chart 
+    let layout = {
+        title: "Top 10 OTUs Found in Individual",
+        xaxis: {title: "Sample Values"},
+        yaxis: {title: "OTU IDs"}
+    };
+
+    // Plot bar chart
+    Plotly.newPlot("bar",trace, layout)
+};
+
+
+
+
+
 
 
 
